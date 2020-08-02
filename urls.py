a878d46430c732e7a1c362a2fbc056ead99b77a5
shreesha07 <69112400+shreesha07@users.noreply.github.com>
2020-08-02 19:23:41 +0530

    urls.py created for myshop

---
+++ urls.py
@@ -0,0 +1,9 @@
+from django.apps import apps
+from django.contrib import admin
+from django.urls import include, path
+
+urlpatterns = [
+    path('i18n/', include('django.conf.urls.i18n')),
+    path('admin/', admin.site.urls),
+     path('', include(apps.get_app_config('oscar').urls[0])),
+]   
